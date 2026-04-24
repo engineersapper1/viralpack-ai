c9387680ff3d2c2145ad4a2c7ccd2b3db6ddc35b
Daniel Kelly <EngineerSapper1@gmail.com>
2026-04-23 22:28:02 -0400

    remove duplicate public routes

--- app/generator/page.jsx
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import GeneratorClient from "./GeneratorClient";
-
-export default async function GeneratorPage() {
-  const cookieStore = await cookies();
-  if (!cookieStore.get("vp_beta")) redirect("/");
-  return <GeneratorClient />;
-}
